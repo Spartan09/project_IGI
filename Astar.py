e76806869457000508852bc35da008e54b96b5de
Spartan09 <31277910+Spartan09@users.noreply.github.com>
2019-07-20 18:05:46 +0530

    Update Astar.py

--- Astar.py
+++ Astar.py
@@ -1,166 +1,256 @@
-inputMatrix = [[1,2,3],[5,6,0],[7,8,4]]
-outputMatrix = [[1,2,3],[5,8,6],[0,7,4]]
+inputMatrix = [[1, 2, 3], [5, 6, 0], [7, 8, 4]]
+outputMatrix = [[1, 2, 3], [5, 8, 6], [0, 7, 4]]
+
 
 class Astar:
+
 	def __init__(self):
 		self.grid = []
-		self.flag = ()
-		self.gCost = None
-		self.hCost = None
-		self.fCost = None
+		self.flag = (-1, -1)
+		self.gCost = 0
+		self.hCost = 0
+		self.fCost = 0
 
-	def findHeuristicValue(inputMatrix, outputMatrix):
-		h = 0
-		for i in range(0, len(inputMatrix)):
-			for j in range(0, len(inputMatrix)):
-				if inputMatrix[i][j] != outputMatrix[i][j]:
-					h += 1
-		return h
 
-	#def calFCost(inputMatrix):
-	#	return (inputMatrix.gCost + inputMatrix.hCost)
+def generateNeighbours(inMatrix):
+	slot = ()
+	neighbours = []
+	for i in range(0, len(inMatrix.grid)):
+		for j in range(0, len(inMatrix.grid)):
+			if inMatrix.grid[i][j] == 0:
+				slot = (i, j)
+				break
 
-	def findBestMatrix(openList):
-		current = []
-		currentFCost = float("inf")
-		for matrix in openList:
-			if matrix.fCost < currentFCost:
-				currentFCost = matrix.fCost
+	x, y = slot
+	px, py = inMatrix.flag
+	delx = x - px
+	dely = y - px
+
+	if px == -1 and py == -1:
+		if x - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x - 1][y] = tobj.grid[x - 1][y], tobj.grid[x][y]
+			tobj.flag = (x - 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		if x + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x + 1][y] = tobj.grid[x + 1][y], tobj.grid[x][y]
+			tobj.flag = (x + 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("-------------------------------------------------check neighbours " + str(
+				[x.grid for x in neighbours]))
+		if y - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y - 1] = tobj.grid[x][y - 1], tobj.grid[x][y]
+			tobj.flag = (x, y - 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("+++++++++++++++++++++++++++++++check neighbours " + str([x.grid for x in neighbours]))
+		if y + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y + 1] = tobj.grid[x][y + 1], tobj.grid[x][y]
+			tobj.flag = (x, y + 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+	if delx < 0 and dely == 0:
+		if x - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x - 1][y] = tobj.grid[x - 1][y], tobj.grid[x][y]
+			tobj.flag = (x - 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+
+		elif y + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y + 1] = tobj.grid[x][y + 1], tobj.grid[x][y]
+			tobj.flag = (x, y + 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif y - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y - 1] = tobj.grid[x][y - 1], tobj.grid[x][y]
+			tobj.flag = (x, y - 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+
+	if delx == 0 and dely < 0:
+		if x - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x - 1][y] = tobj.grid[x - 1][y], tobj.grid[x][y]
+			tobj.flag = (x - 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif x + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x + 1][y] = tobj.grid[x + 1][y], tobj.grid[x][y]
+			tobj.flag = (x + 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif y - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y - 1] = tobj.grid[x][y - 1], tobj.grid[x][y]
+			tobj.flag = (x, y - 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+
+	if delx > 0 and dely == 0:
+		if x + 1 >= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x + 1][y] = tobj.grid[x + 1][y], tobj.grid[x][y]
+			tobj.flag = (x + 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif y + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y + 1] = tobj.grid[x][y + 1], tobj.grid[x][y]
+			tobj.flag = (x, y + 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif y - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y - 1] = tobj.grid[x][y - 1], tobj.grid[x][y]
+			tobj.flag = (x, y - 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+
+	if delx == 0 and dely > 0:
+		if x - 1 >= 0:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x - 1][y] = tobj.grid[x - 1][y], tobj.grid[x][y]
+			tobj.flag = (x - 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif x + 1 <= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x + 1][y] = tobj.grid[x + 1][y], tobj.grid[x][y]
+			tobj.flag = (x + 1, y)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+		elif y + 1 >= 2:
+			tobj = Astar()
+			tobj.grid = inMatrix.grid.copy()
+			tobj.grid[x][y], tobj.grid[x][y + 1] = tobj.grid[x][y + 1], tobj.grid[x][y]
+			tobj.flag = (x, y + 1)
+			tobj.gCost += 1
+			tobj.hCost = findHeuristicValue(tobj.grid)
+			tobj.fCost = tobj.gCost + tobj.hCost
+			neighbours.append(tobj)
+			print("check neighbours " + str([x.grid for x in neighbours]))
+
+	print("check neighbours " + str([x.grid for x in neighbours]))
+	return neighbours
+
+
+def findHeuristicValue(inMatrix):
+	h = 0
+	for i in range(0, len(inMatrix)):
+		for j in range(0, len(inMatrix)):
+			if inMatrix[i][j] != outputMatrix[i][j]:
+				h += 1
+	print("check hval " + str(h))
+	return h
+
+
+def findBestMatrix(current, openList):
+	currentfcost = current.fCost
+	flag = True
+	for matrix in openList:
+		if matrix.fCost < currentfcost:
+			currentfcost = matrix.fCost
+			flag = False
+			print("inside best if " + str(matrix.fCost))
+		print("outside best if " + str(current.grid))
+	if not flag:
 		return matrix
-
-	def generateNeighbours(inputMatrix, depth):
-		slot = ()
-		neighbours = []
-		depth = 0
-		for i in range(0,len(inputMatrix.grid)):
-			for j in range(0,len(inputMatrix.grid)):
-				if inputMatrix.grid[i][j] == 0:
-					slot = (i,j)
-					break
-		x,y = slot
-		px,py = inputMatrix.flag
-		delx = x - px
-		dely = y - px
-		depth += 1
-
-		
-		if delx < 0 and dely == 0:
-			if x - 1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x-1][y] = tempMatrixObj[x-1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x-1, y)
-				neighbours.append(tempMatrixObj)
-			elif y+1 <= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y+1] = tempMatrixObj[x][y+1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y+1)
-				neighbours.append(tempMatrixObj)
-			elif y-1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y-1] = tempMatrixObj[x][y-1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y-1)
-				neighbours.append(tempMatrixObj)
-
-		if delx == 0 and dely < 0:
-			if  x-1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x-1][y] = tempMatrixObj[x-1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x-1, y)
-				neighbours.append(tempMatrixObj)
-			elif x+1 <= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x+1][y] = tempMatrixObj[x+1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x+1, y)
-				neighbours.append(tempMatrixObj)
-			elif y-1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y-1] = tempMatrixObj[x][y-1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y-1)
-				neighbours.append(tempMatrixObj)
+	else:
+		return current
 
 
-		if delx > 0 and dely == 0:
-			if 	x+1 >= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x+1][y] = tempMatrixObj[x+1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x+1, y)
-				neighbours.append(tempMatrixObj)
-			elif y+1 <= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y+1] = tempMatrixObj[x][y+1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y+1)
-				neighbours.append(tempMatrixObj)
-			elif y-1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y-1] = tempMatrixObj[x][y-1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y-1)
-				neighbours.append(tempMatrixObj)
+def pathFinder():
+	openList = []
+	closedList = []
+	current = Astar()
+	current.grid = inputMatrix
+	openList.append(current)
+	checklist = [x.grid for x in openList]
+	print(checklist)
+	while True:
+		current = findBestMatrix(current, openList)
+		print("-----before---rem" + str(current.grid))
+		openList.remove(current)
+		print("-----after---rem" + str(current.grid))
+		closedList.append(current)
+
+		if findHeuristicValue(current.grid) == 0:
+			returnStatement = (current.grid, current.gCost)
+			return returnStatement
+		print("-----before---nhb" + str(current.grid))
+		neighbourList = generateNeighbours(current)
+		print("--nhb list---" + str(neighbourList))
+		for neighbour in neighbourList:
+			if neighbour not in openList:
+				openList.append(neighbour)
+		print("-----after---nhb" + str(current.grid))
 
 
-		if delx == 0 and dely > 0:
-			if 	x-1 >= 0:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x-1][y] = tempMatrixObj[x-1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x-1, y)
-				neighbours.append(tempMatrixObj)
-			elif x+1 <= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x+1][y] = tempMatrixObj[x+1][y], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x+1, y)
-				neighbours.append(tempMatrixObj)
-			elif y+1 >= 2:
-				tempMatrixObj = Astar()
-				tempMatrixObj.grid = inputMatrix.grid.copy()
-				tempMatrixObj[x][y], tempMatrixObj[x][y+1] = tempMatrixObj[x][y+1], tempMatrixObj[x][y]
-				tempMatrixObj.flag = (x, y+1)
-				neighbours.append(tempMatrixObj)
-
-		for tempMatrixObj in neighbours:
-			tempMatrixObj.gCost = depth
-			tempMatrixObj.hCost = findHeuristicValue(tempMatrixObj.grid)
-			tempMatrixObj.fCost = tempMatrixObj.gCost + tempMatrixObj.hCost
-
-		return neighbours
-			
-		
-
-	def pathFinder():
-		openList = []
-		closedList = []
-		current = []
-		depth = 0
-		returnStatement = ()
-		inputMatrixObj = Astar()
-		inputMatrixObj.grid = inputMatrix
-		openList.append(inputMatrixObj)
-
-		while true:
-			current = findBestMatrix(openList)
-			openList.remove(current)
-			closedList.append(current)
-
-			if findHeuristicValue(current.grid) == 0:
-				returnStatement(current.grid, current.depth)
-				return returnStatement
-
-			if openList is empty:
-				return
-
-			for neighbour in generateNeighbours(current):
-				if neighbour not in openList:
-					openList.append(neighbour)
-
-Astar.pathFinder()	
-	
-
+print(pathFinder())
